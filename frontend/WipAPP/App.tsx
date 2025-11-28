8db6072668ea8233a48cbf66828bb70679010f1e
Anthony Abril <antabrrue@alu.edu.gva.es>
2025-11-28 13:48:50 +0100

    frontend scroll solucionado para cualquier screen

--- frontend/WipAPP/App.tsx
+++ frontend/WipAPP/App.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { 
   StyleSheet, 
   Text, 
@@ -12,6 +11,7 @@ import {
 import { useState } from 'react'; // Hook para estado (como BehaviorSubject)
 
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 
@@ -24,15 +24,13 @@ import login from './auth/login'
 import register from './auth/register'
 import HomeScreen from './principal/HomeScreen'
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator<RootStackParamList>();
 
 // En App.tsx y las pantallas
 import { RootStackParamList } from './types/navigation';
 import LinkCarScreen from './principal/LinkCarScreen';
 import CarHomeScreen from './principal/CarHomeScreen';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import TestScrollScreen from './TestScrollScreen';
 
 // Crear el navigator con el tipo = RouterModule en Angular
@@ -47,117 +45,120 @@ export default function App() {
   console.log("App loaded");
   
   return(
-    <View style={styles.appContainer}>
-      <NavigationContainer >
+    
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer >
 
-        <Stack.Navigator
-
-          /*
-          initialRouteName="Onboarding"  // 👈 Empieza con onboarding
-          */
-
-          initialRouteName="Onboarding" //Rutaa inicial
-          
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: '#CCCCC',
-            },
-            headerTintColor: 'white',
-            headerTitleStyle: {
-              fontWeight: 'bold'
-            },
-            headerShown: false,
+          <StatusBar/>
+          <Stack.Navigator
             /*
-            animation: 'reveal_from_bottom',               // 👈 Animación suave
-            transitionSpec: {               // 👈 Configuración avanzada de animación
-              open: { animation: 'timing', config: { duration: 300 } },
-              close: { animation: 'timing', config: { duration: 300 } }
-            },
+            initialRouteName="Onboarding"  // 👈 Empieza con onboarding
             */
-          }}
+
+            initialRouteName="Onboarding" //Rutaa inicial
+            
+            screenOptions={{
+              cardStyle: {
+                  flex: 1
+                },
+              headerStyle: {
+                backgroundColor: '#CCCCC',
+              },
+              headerTintColor: 'white',
+              headerTitleStyle: {
+                fontWeight: 'bold'
+              },
+              headerShown: false,
+              animation: 'reveal_from_bottom',                // 👈 Animación suave
+              transitionSpec: {               // 👈 Configuración avanzada de animación
+                open: { animation: 'timing', config: { duration: 300 } },
+                close: { animation: 'timing', config: { duration: 300 } }
+              }
+            }}>
+            
+
+          {/* Todas estas pantallas heredarán headerShown: false */}
+
           
-        >
+          {/* ✅ Asegúrate de que esta línea esté presente */}
+          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
 
-        {/* Todas estas pantallas heredarán headerShown: false */}
+          <Stack.Screen
+            name="PruebaPrincipal"
+            component={PruebaPrincipal}
+            options={{ title: 'Inicio' }}
+          />
 
-        
-        {/* ✅ Asegúrate de que esta línea esté presente */}
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+          <Stack.Screen
+            name="Prueba1"
+            component={Prueba1}
+            options={{ title: 'Detalles'}}
+          />
 
-        <Stack.Screen
-          name="PruebaPrincipal"
-          component={PruebaPrincipal}
-          options={{ title: 'Inicio' }}
-        />
+          <Stack.Screen
+            name="PlantillaPruebas"
+            component={PlantillaPruebas}
+            options={{ 
+              title: 'plantilla de pruebas',
+              headerShown: false
+            }}
+          />
 
-        <Stack.Screen
-          name="Prueba1"
-          component={Prueba1}
-          options={{ title: 'Detalles'}}
-        />
+          <Stack.Screen
+            name="Login"
+            component={login}
+            options={{ title: 'login'}}
+          />
 
-        <Stack.Screen
-          name="PlantillaPruebas"
-          component={PlantillaPruebas}
-          options={{ 
-            title: 'plantilla de pruebas',
-            headerShown: false
-          }}
-        />
+          <Stack.Screen
+            name="Register"
+            component={register}
+            options={{ title: 'register'}}
+          />
 
-        <Stack.Screen
-          name="Login"
-          component={login}
-          options={{ title: 'login'}}
-        />
+          
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ 
+              headerShown: false // 👈 Asegúrate que esté en false
+            }}
+          />
 
-        <Stack.Screen
-          name="Register"
-          component={register}
-          options={{ title: 'register'}}
-        />
+          <Stack.Screen
+            name="LinkCar"
+            component={LinkCarScreen} // Crear esta pantalla después
+            options={{ 
+              title: 'Vincular Coche',
+              headerShown: true
+            }}
+          />
 
-        
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ 
-            headerShown: false // 👈 Asegúrate que esté en false
-          }}
-        />
-
-        <Stack.Screen
-          name="LinkCar"
-          component={LinkCarScreen} // Crear esta pantalla después
-          options={{ 
-            title: 'Vincular Coche',
-            headerShown: true
-          }}
-        />
-
-        <Stack.Screen
-          name="CarHome"
-          component={CarHomeScreen} // Crear esta pantalla después
-          options={{ 
-            headerShown: true,
-            title: 'Mi Coche'
-          }}
-        />
+          <Stack.Screen
+            name="CarHome"
+            component={CarHomeScreen} // Crear esta pantalla después
+            options={{ 
+              headerShown: true,
+              title: 'Mi Coche'
+            }}
+          />
 
 
 
-        <Stack.Screen
-          name="TestScroll"
-          component={TestScrollScreen} // Crear esta pantalla después
-          options={{ 
-            headerShown: false,
-            title: 'Mi Coche'
-          }}
-        />
-        </Stack.Navigator>
+          <Stack.Screen
+            name="TestScroll"
+            component={TestScrollScreen} // Crear esta pantalla después
+            options={{ 
+              headerShown: false,
+              title: 'Mi Coche'
+            }}
+          />
+          </Stack.Navigator>
 
-      </NavigationContainer>
-    </View>
+        </NavigationContainer>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
